f726fa427bc4e34c5fb6d421f2a0e1db344fc35c
GuilhermeWerner <26710260+GuilhermeWerner@users.noreply.github.com>
2021-07-04 11:49:40 -0300

    Format files and fix about col

--- Codigo/Scripts/wpp-button.js
+++ Codigo/Scripts/wpp-button.js
@@ -1,16 +1,16 @@
-function wppShow(){
+function wppShow() {
     var date = new Date();
     var hour = date.getHours();
     var minute = date.getMinutes();
     document.getElementById("wpp-chat-time").innerText = hour + ":" + minute;
-    $(".wpp-area").css("display","flex");
+    $(".wpp-area").css("display", "flex");
     $(".wpp-area").animate({
         top: '-330px',
         opacity: '100%'
     });
 };
 
-function wppHide(){
+function wppHide() {
     $(".wpp-area").animate({
         top: "-300px",
         opacity: "0%",
